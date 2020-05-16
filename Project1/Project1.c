36badbb71aebeb374e8ff6ffc47c634423b23692
Anton <towkarus@gmail.com>
2020-05-17 00:08:12 +0300

    Заливка фона

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -3,5 +3,22 @@
 
 int main(int argc, char* argv[])
 {
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	window = SDL_CreateWindow("Project1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
 
 }
