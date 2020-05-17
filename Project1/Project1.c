3b45b74c39e43869cda29a9815a55ab52cb8b7c9
Anton <towkarus@gmail.com>
2020-05-17 20:06:59 +0300

    Left, Up, Right, Down

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include "SDL.h"
 
-int processEvents(SDL_Window* window)
+typedef struct
+{
+	int x, y;
+	short life;
+	char* name;
+} Man;
+
+int processEvents(SDL_Window* window, Man* man)
 {
 	SDL_Event event;
 	int done = 0;
@@ -24,6 +31,18 @@ int processEvents(SDL_Window* window)
 			case SDLK_ESCAPE:
 				done = 1;
 				break;
+			case SDLK_LEFT:
+				man->x -= 10;
+				break;
+			case SDLK_UP:
+				man->y -= 10;
+				break;
+			case SDLK_RIGHT:
+				man->x += 10;
+				break;
+			case SDLK_DOWN:
+				man->y += 10;
+				break;
 			}
 			break;
 		case SDL_QUIT:
@@ -35,12 +54,12 @@ int processEvents(SDL_Window* window)
 	return done;
 }
 
-void doRender(SDL_Renderer* renderer)
+void doRender(SDL_Renderer* renderer, Man* man)
 {
-	SDL_SetRenderDrawColor(renderer, 255, 127, 255, 255);
+	SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
 	SDL_RenderClear(renderer);
 	SDL_SetRenderDrawColor(renderer, 255, 127, 127, 255);
-	SDL_Rect rect = { 100, 100, 200, 200 };
+	SDL_Rect rect = { man->x, man->y, 200, 200 };
 	SDL_RenderFillRect(renderer, &rect);
 	SDL_RenderPresent(renderer);
 	SDL_Delay(100);
@@ -53,14 +72,20 @@ int main(int argc, char* argv[])
 	SDL_Renderer* renderer;
 	SDL_Event event;
 	int done = 0;
+	Man man;
 
 	window = SDL_CreateWindow("Project1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+	man.x = 100;
+	man.y = 100;
+	man.name = "Man";
+	man.life = 10;
+
 	while (!done)
 	{
-		done = processEvents(&window);
-		doRender(&renderer);
+		done = processEvents(window, &man);
+		doRender(renderer, &man);
 	}
 
 	SDL_DestroyWindow(window);
