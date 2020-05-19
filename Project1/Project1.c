3e0429ed0cccbf89e45ddd31f37c3b7b45f5db73
Anton <towkarus@gmail.com>
2020-05-20 00:04:02 +0300

    doEvents, doRender, doCollisions

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -1,96 +1,28 @@
 #include <stdio.h>
 #include "SDL.h"
 
-typedef struct
+void doEvents()
 {
-	int x, y;
-	short life;
-	char* name;
-} Man;
 
-int processEvents(SDL_Window* window, Man* man)
-{
-	SDL_Event event;
-	int done = 0;
-
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_WINDOWEVENT_CLOSE:
-			if (window)
-			{
-				SDL_DestroyWindow(window);
-				window = NULL;
-				done = 1;
-			}
-			break;
-		case SDL_KEYDOWN:
-			switch (event.key.keysym.sym)
-			{
-			case SDLK_ESCAPE:
-				done = 1;
-				break;
-			case SDLK_LEFT:
-				man->x -= 10;
-				break;
-			case SDLK_UP:
-				man->y -= 10;
-				break;
-			case SDLK_RIGHT:
-				man->x += 10;
-				break;
-			case SDLK_DOWN:
-				man->y += 10;
-				break;
-			}
-			break;
-		case SDL_QUIT:
-			done = 1;
-			break;
-		}
-	}
-
-	return done;
 }
 
-void doRender(SDL_Renderer* renderer, Man* man)
+void doRendering()
 {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-	SDL_RenderClear(renderer);
-	SDL_SetRenderDrawColor(renderer, 255, 127, 127, 255);
-	SDL_Rect rect = { man->x, man->y, 10, 10 };
-	SDL_RenderFillRect(renderer, &rect);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(100);
+
 }
 
+void doCollisions()
+{
+
+}
 
 int main(int argc, char* argv[])
 {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
-	SDL_Event event;
-	int done = 0;
-	Man man;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
 
 	window = SDL_CreateWindow("Project1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 256, 240, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	man.x = 100;
-	man.y = 100;
-	man.name = "Man";
-	man.life = 10;
-
-	while (!done)
-	{
-		done = processEvents(window, &man);
-		doRender(renderer, &man);
-	}
-
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-	SDL_Quit();
-
-	return 0;
 }
